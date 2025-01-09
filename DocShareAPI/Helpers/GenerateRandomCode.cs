5f1783686f8a9190571a840ac050396080005ac9
Trần Vũ Quang Trường <trutruong2503@gmail.com>
2025-01-09 20:04:42 +0700

    update

---
+++ DocShareAPI/Helpers/GenerateRandomCode.cs
@@ -0,0 +1,13 @@
+﻿namespace DocShareAPI.Helpers
+{
+    public class GenerateRandomCode
+    {
+        private static Random random = new Random(DateTime.Now.Ticks.GetHashCode());
+        public static int GenerateID()
+        {
+            return random.Next(100000000, 1000000000); // Số ngẫu nhiên 9 chữ số
+        }
+        
+    }
+}
+
